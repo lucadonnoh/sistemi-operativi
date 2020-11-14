b329f6bb6fca04fa7ee117c0e3a063e10c5e7e5f
lucadonnoh <donnoh99@gmail.com>
2020-11-14 13:57:53 +0100

    cose

---
+++ README.md
@@ -0,0 +1,2 @@
+
+file che il prof fa e cagate che provo ma tanto non capisco niente
--- forktest.c
+++ forktest.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
 int main(int argc, char * argv[]) {
 	pid_t pid; 
 	if((pid = fork()) != 0)
